acc9ee80958648de2a31593ec19d091f713c2e84
tuffk <tuffkmulhall@gmail.com>
2016-08-23 08:14:40 -0500

    Tarea 2
    si areil aun estoy a tiempo

--- tarea 2.hs
+++ tarea 2.hs
@@ -7,3 +7,5 @@ quicky (x:xs) = let graterPivot = quicky [a | a <- xs, a > x]
 
 
 --newton 
+raiz x guess 0 = guess
+raiz x guess n = (if n <= 1 then newguess else raiz x newguess (n-1)) where newguess = guess - ((guess*guess - x)/(2*guess))
