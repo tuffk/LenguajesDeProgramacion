8c2a17197cd8f734027261a8aeaebe5861412da8
tuffk <tuffkmulhall@gmail.com>
2016-09-06 09:57:30 -0500

    ejercicios prexamen

---
+++ calculo labda.hs
@@ -0,0 +1,10 @@
+--calculo labda
+--definir con \
+--se peude guardar, pero deja de ser anonima   name = (\x->5)
+
+--(\x -> \y -> \z- > x + y + z)
+-- (\x -> \y -> \z- > x + y + z) 3 3 3
+--(\y -> \z -> 3 + y + z) 3 3
+--(\z-> 3 + 3 + z) 3
+--(3 + 3 + 3)
+-- 9
---
+++ ejercicios_examen.hs
@@ -0,0 +1,9 @@
+--ejercicios en clase
+--import Data.Numbers.Primes
+-- 1
+ej1 = [x | x <- [1..], x `mod`2 ==0,x `mod`3 ==0,x `mod`4 ==0,x `mod`5 ==0,x `mod`6 ==0,x `mod`7 ==0,x `mod`8 ==0,x `mod`9 ==0,x `mod`10 ==0 ,x `mod`11 ==0,x `mod`12 ==0 ,x `mod`13 ==0 ,x `mod`14 ==0 ,x `mod`15 ==0 ,x `mod`16 ==0 ,x `mod`17 ==0,x `mod`18 ==0 ]--,x `mod`19 ==0  ,x `mod`20 ==0]
+--12252240
+-- 2
+--milun = [x | x <- [1..] , isPrime x]
+
+--check (h1:h2:h3:h4:h5:_) = h5
