abfe6498861073d4efc25a3ca905c9f40b202d97
tuffk <tuffkmulhall@gmail.com>
2016-09-09 10:56:59 -0500

    asesoria lambda

--- calculo labda.hs
+++ calculo labda.hs
@@ -8,3 +8,12 @@
 --(\z-> 3 + 3 + z) 3
 --(3 + 3 + 3)
 -- 9
+
+--como maquina de turing 
+--abstraccion y aplicacion 
+--abstraccion = definir, aplicacion aplicas
+--f(x) = Lx
+-- (Lx. x+x)
+--x esta acotada xq esta en la L
+--PRIMERO DESDE ADENTRO HACIA AFUERA
+--LA PALICACION VA DE IZQUIERDA A DERECHA
