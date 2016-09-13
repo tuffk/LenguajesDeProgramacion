353e45f4027411d84d66c84442aa6f08d388d38c
tuffk <tuffkmulhall@gmail.com>
2016-09-13 09:20:11 -0500

    lval y rval

---
+++ copycosntructor.cpp
@@ -0,0 +1,41 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+class Foo
+{
+public:
+	Foo(int nval=0):val(nval){cout << "default constructor"<<endl;};
+	~Foo()=default;
+
+	Foo& operator=(const Foo& foo)
+	{
+		cout << "const copy assigment "<<endl;;
+		return *this;
+	}
+	
+	Foo(const Foo& foo)
+	{
+		cout << "copy consttructor"<<endl;
+	}
+	
+	Foo getFoo()
+	{
+		return *this;
+	}
+
+	private:
+	int val;
+};
+
+int main (void)
+{
+	Foo a;
+	Foo b;
+	Foo c(a);
+	b = a;
+	Foo d =b;
+	Foo e = b.getFoo();
+	return 0; 
+}
