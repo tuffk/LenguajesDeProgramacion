a8f57776e2e6275253267a9462c80f0230782387
tuffk <tuffkmulhall@gmail.com>
2016-10-25 09:56:58 -0500

    examen 2 p

--- examen2p.pl
+++ examen2p.pl
@@ -1,11 +1,22 @@
 % examen
-%fibonacci
+%fibonacci con suma mamadora
+concatenar([],L,L).
+concatenar([A|Ca],L2,[A|Cc]):- concatenar(Ca,L2,Cc).
 fib(0,0).
 fib(1,1).
-%fib(N,F) :- N>1, N1 is N-1, N2 is N-2, fib(N1,F1), fib(N2,F2), F is F1+F2.
-fib(N,F) :- N>1, N1 is N-1, N2 is N-2, fib(N1,F1), fib(N2,F2), F is F1+F2,parea(F,S).
-iseven(N,X) :- X is N mod 2.
-parea(F,S):- iseven(F,N), N ==0, [N|S].
+fib(N,Y):-N>1,N1 is N-1,fib(N1,Y1),N2 is N-2,fib(N2,Y2),Y is Y1+Y2.
+serie(0,[0]).
+serie(1,[1]).
+serie(Nt,L):- Nt>1,fib(Nt,P),N1 is Nt-1,serie(N1,Ls),concatenar(Ls,[P],L).
+
+%sum(N,L,R):- L2 is   .
+sum([], 0).
+
+sum([X|Xs], S):-
+          sum(Xs, S2),R1 is S2 mod 2, R2 is X mod 2, (R1 == 0 -> (R2 == 0 -> S is S2 + X; S is S2)).
+
+serieFib(N,L,R):- serie(N,L), sum(L, R).
+
 %hanoi 
 hanoi(N) :- muevemela(N,izquierda,centro,derecha).
  
@@ -24,7 +35,7 @@ ingles(program,programa).
 espa(de,of).
 espa(como,how).
 espa(funciona,works).
-
+% trducemela con mil y un !
 traducemela([],E,I).
 %traducemela([H|T],E,I) :- (ingles(H,X);espa(H,Y)),traducemela(T,[Y],[X]),imprimela(E,I).
 traducemela([H|T],E,I) :- (ingles(H,X),traducemela(T,E,[X|I]),!;espa(H,Y),traducemela(T,[Y|E],I),!),imprimela(E,I).
